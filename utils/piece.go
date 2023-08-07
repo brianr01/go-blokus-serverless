c1f3bf6005de609991b298bd62cb12a2306c895c
Brian Ronstrom <brianr.email1@gmail.com>
2023-08-07 12:06:56 -0400

    Moved piece helpers into utils.

--- utils/piece.go
+++ utils/piece.go
@@ -2,6 +2,10 @@ package utils
 
 import (
 	"fmt"
+	"image"
+	"log"
+	"math"
+	"reflect"
 
 	"github.com/brianr01/go-blockus-serverless/helpers"
 	"github.com/brianr01/go-blockus-serverless/types"
@@ -21,8 +25,8 @@ func CreatePieceDetails(fileName string, folderLocation string) types.PieceDetai
 
 	image := helpers.GetImageFromFile(fmt.Sprintf("%s/%s", folderLocation, fileName))
 
-	ridgidPiece := helpers.CreateRidigPieceFromImage(image)
-	dimensions := helpers.GetDimensionsFromRidigPiece(ridgidPiece)
+	ridgidPiece := CreateRidigPieceFromImage(image)
+	dimensions := GetDimensionsFromRidigPiece(ridgidPiece)
 	symmetries := GetValidSymmetriesFromRidgidPiece(ridgidPiece)
 
 	pieceDetails := types.PieceDetails{
@@ -44,12 +48,12 @@ func GetValidSymmetriesFromRidgidPiece(p types.RidgidPiece) []types.Symmetry {
 		for m := 0; m < 2; m++ { // 2 mirrors
 			mirrored := m == 1
 
-			rp := helpers.RotateRidgidPiece(p, r*90)
+			rp := RotateRidgidPiece(p, r*90)
 			if mirrored {
 				rp = helpers.ReverseRows2d(rp)
 			}
 
-			if !helpers.RidgidPieceInRidgidPieces(rp, validRidigPieces) {
+			if !RidgidPieceInRidgidPieces(rp, validRidigPieces) {
 				validRidigPieces = append(validRidigPieces, rp)
 				symmetries = append(symmetries, types.Symmetry{
 					Mirror:      mirrored,
@@ -61,4 +65,80 @@ func GetValidSymmetriesFromRidgidPiece(p types.RidgidPiece) []types.Symmetry {
 	}
 
 	return symmetries
+}
+
+func CreateRidigPieceFromImage(img image.Image) types.RidgidPiece {
+	bX := img.Bounds().Max.X
+	bY := img.Bounds().Max.Y
+
+	p := make(types.RidgidPiece, bX)
+
+	for x := 0; x < bX; x++ {
+		p[x] = make([]int, bY)
+
+		for y := 0; y < bY; y++ {
+			if helpers.IsPixelWhite(x, y, img) {
+				p[x][y] = 0
+				continue
+			}
+
+			p[x][y] = 1
+		}
+	}
+
+	return p
+}
+
+func GetDimensionsFromRidigPiece(p types.RidgidPiece) [2]int {
+	w := len(p)
+
+	if w == 0 {
+		log.Fatalf("Cannot get dimensions fom ridig piece that has no width.")
+	}
+
+	h := len(p[0])
+
+	return [2]int{w, h}
+}
+
+func RotateRidgidPiece(p types.RidgidPiece, r int) types.RidgidPiece {
+	if !helpers.IsRotationValidFor90Degrees(r) {
+		log.Fatalf("Cannot rotate ridgid piece.  Invalid rotation %v", r)
+	}
+
+	rotateClockWise := r >= 0
+
+	rotations := int(math.Abs(float64(r / 90)))
+
+	if rotateClockWise {
+		for i := 0; i < rotations; i++ {
+			p = RotateRidigidPieceClockwise(p)
+		}
+
+		return p
+	}
+
+	for i := 0; i < rotations; i++ {
+		p = RotateRidigidPieceCounterClockwise(p)
+	}
+
+	return p
+}
+
+func RotateRidigidPieceCounterClockwise(p types.RidgidPiece) types.RidgidPiece {
+	return helpers.ReverseRows2d(helpers.Transpose2d(p))
+}
+
+func RotateRidigidPieceClockwise(p types.RidgidPiece) types.RidgidPiece {
+	return helpers.ReverseColumns2d(helpers.Transpose2d(p))
+}
+
+func RidgidPieceInRidgidPieces(p1 types.RidgidPiece, ps []types.RidgidPiece) bool {
+	for _, p2 := range ps {
+		if reflect.DeepEqual(p1, p2) {
+			return true
+		}
+	}
+
+	return false
 }
